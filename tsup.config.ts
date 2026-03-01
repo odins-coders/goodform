08a9b5560d4cbbceea12e384f94f3eb507c25184
Jason Hughes <jasonhughes1@outlook.com>
2026-03-01 13:41:56 +1000

    starting again

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   dts: true,
   sourcemap: true,
   clean: true,
-  external: ["react", "react-dom", "react/jsx-runtime", "effect"],
+  external: ["react", "react-dom", "react/jsx-runtime"],
 })
